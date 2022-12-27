408e39ce27a15f019d490f544cc6af117b97ac08
euhansarkar <euhansarkar@gmail.com>
2022-12-28 02:43:36 +0600

    new post section added

--- src/Pages/Home/NewPost/NewPost.jsx
+++ src/Pages/Home/NewPost/NewPost.jsx
@@ -1,10 +1,22 @@
 import React from "react";
 import { useForm } from "react-hook-form";
-import {FaVideo} from 'react-icons/fa';
-import {IoMdPhotos} from "react-icons/io"
-import {BsEmojiSmile} from 'react-icons/bs'
+import { FaVideo } from "react-icons/fa";
+import { IoMdPhotos } from "react-icons/io";
+import { BsEmojiSmile } from "react-icons/bs";
+import PostModal from "../PostModal/PostModal";
 const NewPost = () => {
   const { register, handleSubmit } = useForm();
+
+  const myPostModal = () => {
+    return (
+      <div>
+        <label htmlFor="postModal" className="btn">
+          open modal
+        </label>
+        {console.log(`clicked`)}
+      </div>
+    );
+  };
 
   return (
     <div className="bg-gray-200 rounded-md p-5">
@@ -12,23 +24,25 @@ const NewPost = () => {
         <div className="container flex items-center space-x-3 mx-3">
           <div className="avatar">
             <div className="w-16 rounded-full">
-              <img src="https://placeimg.com/192/192/people" />
+              <img src="https://placeimg.com/192/192/people" alt="" />
             </div>
           </div>
-          <div className="form-control w-full">
-            <input
-              {...register("firstName")}
-              type="text"
-              placeholder="what's on your mind! euhan?"
-              className="input input-bordered w-full"
-            />
-          </div>
+          <label htmlFor="postModal" className="input input-bordered w-full ">
+            what's on your mind euhan ?
+          </label>
         </div>
         <div className="flex items-center justify-evenly w-full">
+          <label htmlFor="postModal">
             <FaVideo className="text-3xl text-red-700" />
+          </label>
+          <label htmlFor="postModal">
             <IoMdPhotos className="text-3xl text-green-700" />
-            <BsEmojiSmile className="text-3xl text-amber-400" />
+          </label>
+          <label htmlFor="postModal">
+            <BsEmojiSmile className="text-3xl text-amber-400"></BsEmojiSmile>
+          </label>
         </div>
+        <PostModal />
       </form>
     </div>
   );
